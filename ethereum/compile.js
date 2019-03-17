f71844b3faf40fa4318484db8dbc480886f25d24
mr-fool <mr-fool@users.noreply.github.com>
2019-03-16 23:44:09 -0600

    compile script

---
+++ ethereum/compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname,'contracts','Campaign.sol');
+var input = {
+	language: 'Solidity',
+	sources: {
+		'Campaign.sol': {
+			content:  fs.readFileSync("contracts/Campaign.sol",'utf-8')
+		}
+	},
+	settings: {
+		outputSelection: {
+			'*': {
+				'*': [ '*' ]
+			}
+		}
+	}
+}
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
