1da2f7214b6c90cff6f4b004e29a0b36929ec9e5
mr-fool <mr-fool@users.noreply.github.com>
2019-03-26 17:04:41 -0600

    added error handling

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4bf8b2d07c52bfa4f24b";
+/******/ 	var hotCurrentHash = "503cdba0590f8988d55e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
