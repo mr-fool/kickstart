45a626c98b8eff5743fc04603504c1ba8d2dc080
mr-fool <mr-fool@users.noreply.github.com>
2019-03-27 16:18:25 -0600

    more routing

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9a4f0a2462a2a60e6741";
+/******/ 	var hotCurrentHash = "2be52ecaed01ff870ae2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
