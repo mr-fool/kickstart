4f2ee6e518492a51633cd54473fba89bb9845bcc
mr-fool <mr-fool@users.noreply.github.com>
2019-03-30 12:39:36 -0600

    added style: {overflowWrap: 'break-word'}

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "177de21a91b620a133fb";
+/******/ 	var hotCurrentHash = "fa7389e2f9bafd9a28cf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
